e964623ebece284226db4e9bcd9aa556a999c06e
MD Raza <raza231198@gmail.com>
2021-03-24 12:39:02 +0000

    apollo: add ramdisk files
    Signed-off-by: MD Raza <raza231198@gmail.com>

--- device.mk
+++ device.mk
@@ -6,3 +6,10 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
+
+# FastbootD
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(DEVICE_PATH)/recovery/root,recovery/root)
